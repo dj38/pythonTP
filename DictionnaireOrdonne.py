a682fd5208be5c7ae58a0dc10926632a57404f9a
joel <joel.damiens@laposte.net>
2018-12-08 14:11:14 +0100

    Created DictionnaireOrdonne.py : nouvelle classe

---
+++ DictionnaireOrdonne.py
@@ -0,0 +1,10 @@
+class DictionnaireOrdonne:
+    """
+    Dictionnaire ordonne
+    """
+
+
+    def __init__(self,base=[],**data):
+        self.keyList=[]  # list of keys
+        self.valueList=[]  # list of values
+        
